60516c37cc70f993630a929b2e38f321a3f7d827
Mark Pinches <mark@alderley.ai>
2017-09-10 23:48:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ console.log('Hello welcome to the knx bus monitor package')
 var knx  	= require('knx_eibd');
 
 // import usb logger
-//var usb_logger = require('./node_modules/utilities/usb_logger');
+//require('./node_modules/utilities/usb_logger');
 
 // import send message to bus
 //var sendMsg = require('./node_modules/utilities/send_msg_to_bus').sendMsg; 
@@ -65,22 +65,28 @@ var knx  	= require('knx_eibd');
 
 // import timed jobs for knx. This auto reads from a csv file to make jobs
 // The file with cron jobs is on the usb in knx_jobs folder 
-//var jobs = require('./node_modules/utilities/knx_cron_jobs');
+//require('./node_modules/utilities/knx_cron_jobs');
 
 
 //This runs a bluetooth connection to a sensortag that monitors humidity
 // when humidity runs high the towel rails come on for 30mins
-//var sensortag_bath = require('./node_modules/utilities/sensortag_bath');
+//require('./node_modules/utilities/sensortag_bath');
 
 
 //This initiates a set of scenes associated with a number of group addresses
-//var scenes = require('./node_modules/utilities/scenes');
+//require('./node_modules/utilities/scenes');
 
 // This sets up the door monitor
-//var doors = require('./node_modules/utilities/doors');
+//require('./node_modules/utilities/doors');
 
 // This sets up the tank monitor
 //require('./node_modules/utilities/ultrasound');
 
 //This sets up the main area average generator
-//var temp =require('./node_modules/utilities/main_temp_avg');
+//require('./node_modules/utilities/main_temp_avg');
+
+// This sets up the server...
+//require('./node_modules/server')
+
+//This sets up the key databases used
+//require('./node_modules/utilities/db');
